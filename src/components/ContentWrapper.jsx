354c7bb6b80591da6c253b46d860fedc9ac4140d
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-12-15 11:06:23 +0500

    remain to open navbar on click

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentWrapper = ({ children }) => {
   return (
-    <div className="w-full max-w-[1200px] my-0 mx-auto py-0 px-5 border border-black">
+    <div className="w-full max-w-[1200px] my-0 mx-auto py-0 px-5">
       {children}
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./header.css";
+
 import ContentWrapper from "./ContentWrapper";
 import logo from "../assets/movix-logo.svg";
 
@@ -12,33 +12,82 @@ const Header = () => {
   const [lastScrollY, setLastScrollY] = useState(0);
   const [mobileMenu, setMobileMenu] = useState(false);
   const [query, setQuery] = useState("");
-  const [showSearch, setShowSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+    setMobileMenu(false);
+  };
   return (
-    <header className="fixed translate-y-0 w-full h-[60px] z-10 flex items-center transition duration-100 ease-in-out border-red-200 border-2">
+    <header
+      className={`fixed translate-y-0 w-full h-[60px] z-10 flex items-center transition duration-100 ease-in-out ${show} ${
+        mobileMenu ? "!bg-[#020c1b]" : ""
+      } `}
+    >
       <ContentWrapper>
         <div className=" flex justify-between items-center">
           <div className=" cursor-pointer">
             <img src={logo} alt="logo" className="h-[52px]" />
           </div>
-          <ul className="list-none hidden items-center md:flex">
+          <ul
+            className={` ${
+              mobileMenu
+                ? "flex absolute top-[60px] left-0 bg-black3 flex-col w-full py-5 border-t border-solid border-t-white border-opacity-10 animate-mobileMenu"
+                : "list-none hidden items-center md:flex"
+            }`}
+          >
             <li
-              onClick={() => {}}
-              className="h-[60px] flex items-center mx-4 text-white font-bold relative"
+              className={`h-[60px] flex items-center mx-4 text-white font-bold relative ${
+                mobileMenu && "menuItemMobile"
+              }`}
+              onClick={() => {
+                navigationHandler("movie");
+              }}
             >
               Movies
             </li>
-            <li className="h-[60px] flex items-center mx-4 text-white font-bold relative">
+            <li
+              className={`h-[60px] flex items-center mx-4 text-white font-bold relative ${
+                mobileMenu && "menuItemMobile"
+              }`}
+              onClick={() => {
+                navigationHandler("tv");
+              }}
+            >
               TV Shows
             </li>
-            <li className="h-[60px] flex items-center mx-4 text-white font-bold relative">
+            <li
+              className={`h-[60px] flex items-center mx-4 text-white font-bold relative ${
+                mobileMenu && "hidden"
+              }`}
+            >
               <HiOutlineSearch />
             </li>
           </ul>
-          <div>
+          <div className=" flex items-center gap-5 md:hidden text-white text-lg">
             <HiOutlineSearch />
-            {mobileMenu ? <VscChromeClose /> : <SlMenu />}
+            {mobileMenu ? (
+              <VscChromeClose
+                onClick={() => {
+                  setMobileMenu(false);
+                }}
+              />
+            ) : (
+              <SlMenu onClick={openMobileMenu} />
+            )}
           </div>
         </div>
       </ContentWrapper>
