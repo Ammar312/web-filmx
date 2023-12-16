700456971f649aa63d4b0fc2d2384d2ac857fbb9
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-12-16 10:31:31 +0500

    control navbar working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
 import ContentWrapper from "./ContentWrapper";
 import logo from "../assets/movix-logo.svg";
@@ -11,10 +11,21 @@ const Header = () => {
   const [show, setShow] = useState("top");
   const [lastScrollY, setLastScrollY] = useState(0);
   const [mobileMenu, setMobileMenu] = useState(false);
-  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  });
+
+  const controlNavbar = () => {
+    console.log(window.scrollY);
+  };
   const openMobileMenu = () => {
     setMobileMenu(true);
     setShowSearch(false);
@@ -31,6 +42,14 @@ const Header = () => {
     }
     setMobileMenu(false);
   };
+  const searchHandler = (e) => {
+    if (e.code === "Enter" && search.length > 0) {
+      navigate(`/search/${search}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
   return (
     <header
       className={`fixed translate-y-0 w-full h-[60px] z-10 flex items-center transition duration-100 ease-in-out ${show} ${
@@ -40,17 +59,19 @@ const Header = () => {
       <ContentWrapper>
         <div className=" flex justify-between items-center">
           <div className=" cursor-pointer">
-            <img src={logo} alt="logo" className="h-[52px]" />
+            <Link to="/">
+              <img src={logo} alt="logo" className="h-[52px]" />
+            </Link>
           </div>
           <ul
-            className={` ${
+            className={` cursor-pointer ${
               mobileMenu
                 ? "flex absolute top-[60px] left-0 bg-black3 flex-col w-full py-5 border-t border-solid border-t-white border-opacity-10 animate-mobileMenu"
                 : "list-none hidden items-center md:flex"
             }`}
           >
             <li
-              className={`h-[60px] flex items-center mx-4 text-white font-bold relative ${
+              className={`h-[60px] flex items-center mx-4 text-white font-bold relative hover:text-red-500 ${
                 mobileMenu && "menuItemMobile"
               }`}
               onClick={() => {
@@ -60,7 +81,7 @@ const Header = () => {
               Movies
             </li>
             <li
-              className={`h-[60px] flex items-center mx-4 text-white font-bold relative ${
+              className={`h-[60px] flex items-center mx-4 text-white font-bold relative hover:text-red-500 ${
                 mobileMenu && "menuItemMobile"
               }`}
               onClick={() => {
@@ -70,15 +91,15 @@ const Header = () => {
               TV Shows
             </li>
             <li
-              className={`h-[60px] flex items-center mx-4 text-white font-bold relative ${
+              className={`h-[60px] flex items-center mx-4 text-white font-bold relative hover:text-red-500 ${
                 mobileMenu && "hidden"
               }`}
             >
-              <HiOutlineSearch />
+              <HiOutlineSearch onClick={openSearch} />
             </li>
           </ul>
           <div className=" flex items-center gap-5 md:hidden text-white text-lg">
-            <HiOutlineSearch />
+            <HiOutlineSearch onClick={openSearch} />
             {mobileMenu ? (
               <VscChromeClose
                 onClick={() => {
@@ -91,6 +112,29 @@ const Header = () => {
           </div>
         </div>
       </ContentWrapper>
+      {showSearch && (
+        <div className=" w-full h-[60px] bg-white absolute top-[60px] animate-mobileMenu md:bg-transparent ">
+          <ContentWrapper>
+            <div className=" flex items-center w-full h-10 mt-2">
+              <input
+                type="search"
+                className="w-full h-[50px] bg-white outline-none border-none rounded-[30px 0 0 30px] py-0 px-4 text-sm   md:text-xl md:p-[0px 30px]"
+                placeholder="Search for a movie "
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                onKeyDown={searchHandler}
+              />
+              <VscChromeClose
+                className="text-xl shrink-0 ml-[10px] cursor-pointer md:text-white"
+                onClick={() => {
+                  setShowSearch(false);
+                  setSearch("");
+                }}
+              />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
     </header>
   );
 };
--- src/pages/home/HeroBanner.jsx
+++ src/pages/home/HeroBanner.jsx
@@ -19,8 +19,8 @@ const HeroBanner = () => {
     setBackground(bg);
   }, [data]);
 
-  const searchHandler = (e) => {
-    if (e.code === "Enter" && search.length > 0) {
+  const searchHandler = () => {
+    if (search.length > 0) {
       navigate(`/search/${search}`);
     }
   };
@@ -45,14 +45,16 @@ const HeroBanner = () => {
           <div className=" flex items-center w-full">
             <input
               type="search"
-              className="w-[calc(100%-100px)] h-[50px] bg-white outline-none border-none rounded-[30px 0 0 30px] py-0 px-4 text-sm md:w-[calc(100%-150px)] md:h-[60px] md:text-xl md:p-[0px 30px]"
+              className="w-[calc(100%-100px)] h-[50px] bg-white text-orange outline-none border-none rounded-[30px 0 0 30px] py-0 px-4 text-sm md:w-[calc(100%-150px)] md:h-[60px] md:text-xl md:p-[0px 30px]"
               placeholder="Search for a Movie or TvShow"
               onChange={(e) => setSearch(e.target.value)}
               value={search}
+              onKeyDown={(e) => e.code === "Enter" && searchHandler()}
             />
             <button
               type="submit"
               className="w-[100px] h-[50px] bg-gradient text-white outline-none border-none rounded-[ 0 30px 30px 0] text-base cursor-pointer md:w-[150px] md:h-[60px] md:text-lg"
+              onClick={searchHandler}
             >
               Search
             </button>
