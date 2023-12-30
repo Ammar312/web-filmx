ad727eab91f0c3eb0b6cb55592e65bc118531fa9
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-12-30 08:40:59 +0500

    only send official trailer

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import useFetch from "../../hook/useFetch";
 import DetailBanner from "./DetailBanner";
 
 const Details = () => {
+  const [official, setOfficial] = useState(null);
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading: creditsLoading } = useFetch(
     `/${mediaType}/${id}/credits`
   );
+
+  useEffect(() => {
+    if (data?.results) {
+      for (let v = 0; v < data?.results.length; v++) {
+        if (data?.results?.[v].name === "Official Trailer") {
+          setOfficial(data?.results?.[v]);
+          break;
+        }
+      }
+    }
+  }, [data]);
   console.log("video: ", data);
   return (
     <div>
-      <DetailBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <DetailBanner video={official} crew={credits?.crew} />
     </div>
   );
 };
